25806d484980e795e8fddd1ad891eb93ff5dd9bc
itegulov <ditegulov@gmail.com>
2016-05-04 18:17:09 +0300

    Move all general functions to package.object

--- src/main/scala/at/logic/skeptik/algorithm/prover/UnitPropagation.scala
+++ src/main/scala/at/logic/skeptik/algorithm/prover/UnitPropagation.scala
@@ -2,9 +2,7 @@ package at.logic.skeptik.algorithm.prover
 
 import at.logic.skeptik.expression.E
 import at.logic.skeptik.judgment.Sequent
-import at.logic.skeptik.judgment.immutable.SetSequent
 
-import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks
 
@@ -14,33 +12,6 @@ import scala.util.control.Breaks
   * @author Daniyar Itegulov
   */
 object UnitPropagation {
-  private def isUnit(clause: Sequent): Boolean = clause.width == 1
-
-  // Assumes, that `isUnit` is true
-  private def getUnit(clause: Sequent): (Boolean, E) =
-    if (clause.ant.size == 1) (true, clause.ant.head) else (false, clause.suc.head)
-
-  // Eliminates one unit clause `negate:unit` in `exp`
-  private def eliminate(exp: Set[Sequent], negated: Boolean, unit: E): Set[Sequent] = {
-    val newResult = mutable.Set.empty[Sequent]
-    for (clause <- exp) {
-      if (!(!negated && clause.sucContains(unit)) && !(negated && clause.antContains(unit))) {
-        val newSuc = if (negated) {
-          clause.suc.filter(_ != unit).toSet
-        } else {
-          clause.suc.toSet
-        }
-        val newAnt = if (negated) {
-          clause.ant.toSet
-        } else {
-          clause.ant.filter(_ != unit).toSet
-        }
-        newResult += new SetSequent(newAnt, newSuc)
-      }
-    }
-    newResult.toSet
-  }
-
   /**
     * Applies unit propagation technique to expression.
     *
---
+++ src/main/scala/at/logic/skeptik/algorithm/prover/package.scala
@@ -0,0 +1,39 @@
+package at.logic.skeptik.algorithm
+
+import at.logic.skeptik.expression.E
+import at.logic.skeptik.judgment.Sequent
+import at.logic.skeptik.judgment.immutable.SetSequent
+
+import scala.collection.mutable
+
+/**
+  * @author Daniyar Itegulov
+  */
+package object prover {
+  def isUnit(clause: Sequent): Boolean = clause.width == 1
+
+  // Assumes, that `isUnit` is true
+  def getUnit(clause: Sequent): (Boolean, E) =
+    if (clause.ant.size == 1) (true, clause.ant.head) else (false, clause.suc.head)
+
+  // Eliminates one unit clause `negate:unit` in `exp`
+  def eliminate(exp: Set[Sequent], negated: Boolean, unit: E): Set[Sequent] = {
+    val newResult = mutable.Set.empty[Sequent]
+    for (clause <- exp) {
+      if (!(!negated && clause.sucContains(unit)) && !(negated && clause.antContains(unit))) {
+        val newSuc = if (negated) {
+          clause.suc.filter(_ != unit).toSet
+        } else {
+          clause.suc.toSet
+        }
+        val newAnt = if (negated) {
+          clause.ant.toSet
+        } else {
+          clause.ant.filter(_ != unit).toSet
+        }
+        newResult += new SetSequent(newAnt, newSuc)
+      }
+    }
+    newResult.toSet
+  }
+}
